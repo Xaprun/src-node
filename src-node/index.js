c6d4f7a392a35890cec4ecd38bb791fb8345d2b7
Your Name <you@example.com>
2024-07-26 13:24:01 +0200

    os-yellow files

--- src-node/index.js
+++ src-node/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const Redis = require('ioredis');
+const os = require('os');
+const dns = require('dns');
 
 const app = express();
 const redis = new Redis({
@@ -69,6 +71,44 @@ app.get('/', async (req, res) => {
       </html>
     `);
   }
+});
+
+app.get('/env', (req, res) => {
+  const hostname = os.hostname();
+  const platform = os.platform();
+  const arch = os.arch();
+  const cpus = os.cpus().length;
+  const totalMemory = os.totalmem();
+  const freeMemory = os.freemem();
+
+  dns.lookup(os.hostname(), (err, address) => {
+    if (err) {
+      return res.send(`<h1>Error fetching IP address: ${err.message}</h1>`);
+    }
+
+    res.send(`
+      <!DOCTYPE html>
+      <html>
+      <head>
+        <style>
+          body { background-color: blue; color: white; font-family: Arial, sans-serif; }
+        </style>
+      </head>
+      <body>
+        <h1>Environment Information</h1>
+        <ul>
+          <li>Hostname: ${hostname}</li>
+          <li>IP Address: ${address}</li>
+          <li>OS Platform: ${platform}</li>
+          <li>OS Architecture: ${arch}</li>
+          <li>Number of CPUs: ${cpus}</li>
+          <li>Total Memory: ${totalMemory}</li>
+          <li>Free Memory: ${freeMemory}</li>
+        </ul>
+      </body>
+      </html>
+    `);
+  });
 });
 
 const PORT = 8080;
